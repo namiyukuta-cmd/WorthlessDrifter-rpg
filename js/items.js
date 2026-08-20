d09ab72d5e44fa70ed08986076ba51b845febeb0
namiyukuta-cmd <namiyukuta@gmail.com>
2026-08-20 22:43:46 +0900

    Mark pet food for companion recovery

--- js/items.js
+++ js/items.js
@@ -7,4 +7,4 @@ const baseItems=[
 {id:'bread',name:'パン',icon:'🥖',kind:'food',w:2,h:1,weight:.35,parent:'loose',consume:{type:'food',amount:3,remove:true}},
 {id:'soap',name:'石鹸',icon:'▱',kind:'tool',w:1,h:1,weight:.18,parent:'loose'},
 {id:'pot',name:'小鍋',icon:'◒',kind:'tool',w:2,h:2,weight:1.3,parent:'loose'},
-{id:'petfood',name:'餌袋',icon:'◫',kind:'food',w:2,h:1,weight:.8,parent:'pet',x:0,y:0}];
+{id:'petfood',name:'餌袋',icon:'◫',kind:'food',w:2,h:1,weight:.8,parent:'pet',x:0,y:0,companionFeed:{remove:true}}];
