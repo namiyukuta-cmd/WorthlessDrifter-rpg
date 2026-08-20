ac0f7ca080e2b0b68a92a50c1465ec4708854a73
namiyukuta-cmd <namiyukuta@gmail.com>
2026-08-21 08:18:38 +0900

    Load index layout module

--- worlds/catalog.js
+++ worlds/catalog.js
@@ -2,3 +2,4 @@ const WORLD_CATALOG=[
   {id:'dry-trade-road',name:'乾いた交易路',description:'砂漠と乾燥平原を街道でつなぐ交易圏。小さな村、隊商宿、オアシス、町、遠方の城壁都市がある。'}
 ];
 const WORLD_DATA={};
+loadGameScript('js/index-layout.js').catch(()=>{});
